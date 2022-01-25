5f9d1cc2da0242ac0763ad9495962d781ef5a093
Kavita <kavi.pdri@gmail.com>
2022-01-25 11:08:45 -0800

    dealing cards

--- src/App.js
+++ src/App.js
@@ -1,28 +1,51 @@
 import './App.css';
+import {useState} from 'react';
 const _ = require('underscore');
 
 const App = () => {
 
-
   //build deck
   const deckBuilder = () => {
     let deck = [];
-    ['H', 'C', 'S', 'D'].forEach(suit => {
-      ['a', 2, 3, 4, 5, 6, 7, 8, 9, 10, 'j', 'q', 'k'].forEach(rank => {
-        deck.push(suit + rank);
+    ['Hearts', 'Clubs', 'Spades', 'Diamonds'].forEach(suit => {
+      ['Ace', 2, 3, 4, 5, 6, 7, 8, 9, 10, 'Jack', 'Queen', 'King'].forEach(rank => {
+        deck.push({
+          suit: suit,
+          rank: rank
+        });
       });
     });
     deck = _.shuffle(deck);
-    console.log(deck);
     return deck;
   }
 
   let deck = deckBuilder();
 
+  //control stage
+  const [week, changeWeek] = useState(0);
+
+  const evaluateStage = () => {
+    if (week === 0) {
+      return 'Start';
+    } else if (week >= 4) {
+      return 'End';
+    } else {
+      let card = deck.pop();
+      return (`Week ${week} \n
+      Playing ${card.rank} of ${card.suit}`);
+    }
+  }
+
   return (
     <div className="App">
       <h1>Fire Cycle</h1>
       <h2>A year in the peaks.</h2>
+      <div>
+        {evaluateStage(week)}
+      </div>
+      <button onClick = {() => changeWeek(week + 1)}>
+        Next week
+      </button>
     </div>
   );
 }
