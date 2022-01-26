ac8eddfd80f4811f0319c1a7ce9d2a882cc42a63
Kavita <kavi.pdri@gmail.com>
2022-01-26 12:18:21 -0800

    separating out info for reuse

--- src/Components/EventCard.jsx
+++ src/Components/EventCard.jsx
@@ -1,14 +1,13 @@
+import {suits, suitMeanings} from '../utils/cardText.js';
+
 const EventCard = (props) => {
-  const suits = {
-    'Hearts': 'Blessings',
-    'Diamonds': 'News',
-    'Clubs': 'Fortune',
-    'Spades': 'Edges'
-  }
+
+  // console.log(cardText);
+  console.log(suits);
 
   return (<div className="Event-Card">
     <div className="event text">The {props.card.rank} of {suits[props.card.suit]}.</div>
-    <p className="event-description text">The {props.card.rank} represents {'{PENDING...}'}. The suit of {suits[props.card.suit]} is associated with {'{PENDING...}'}</p>
+    <p className="event-description text">The {props.card.rank} represents {'{PENDING...}'}. The suit of {suits[props.card.suit]} is associated with {suitMeanings[props.card.suit]}.</p>
     <p>What happened, and how did the people respond? What did you think of it all?</p>
   </div>)
 
---
+++ src/utils/cardText.js
@@ -0,0 +1,13 @@
+export const
+  suits = {
+    'Hearts': 'Bonds',
+    'Diamonds': 'News',
+    'Clubs': 'Fortune',
+    'Spades': 'Mysteries'
+  },
+  suitMeanings = {
+    'Hearts': 'connections: between people, places, things, etc',
+    'Diamonds': 'news, of course, primarily from the outside world: about the lowlands, the royal palace, the state of the war, and so on',
+    'Clubs': 'omens, auspices, and lucky or unlucky events',
+    'Spades': 'strange and unexplained events, phenomena no one understands, and maybe a hint of the supernatural'
+  }
